94f742161053072836552d06744ecb3875668470
Philipp <62473021+CrAfTsArMy@users.noreply.github.com>
2025-06-15 08:55:14 +0200

    [CH] Better input validation before parsing

--- json/src/main/java/de/craftsblock/craftscore/json/JsonParser.java
+++ json/src/main/java/de/craftsblock/craftscore/json/JsonParser.java
@@ -3,6 +3,8 @@ package de.craftsblock.craftscore.json;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonPrimitive;
 import de.craftsblock.craftscore.utils.Utils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -20,7 +22,7 @@ import java.util.Arrays;
  *
  * @author Philipp Maywald
  * @author CraftsBlock
- * @version 2.0.3
+ * @version 2.0.4
  * @see Json
  * @see JsonValidator
  * @since 3.6#16-SNAPSHOT
@@ -38,7 +40,7 @@ public final class JsonParser {
      * @return A {@link Json} object representing the content of the file
      * @throws RuntimeException If an I/O error occurs during reading or file creation
      */
-    public static Json parse(File file) {
+    public static @NotNull Json parse(@NotNull File file) {
         return parse(file.toPath());
     }
 
@@ -49,7 +51,7 @@ public final class JsonParser {
      * @return A {@link Json} object representing the content of the path
      * @throws RuntimeException If an I/O error occurs during reading the path
      */
-    public static Json parse(Path path) {
+    public static @NotNull Json parse(@NotNull Path path) {
         try {
             Path parent = path.getParent();
             if (parent != null && Files.notExists(parent)) Files.createDirectories(parent);
@@ -71,7 +73,7 @@ public final class JsonParser {
      * @return A {@link Json} object representing the content of the InputStream
      * @throws RuntimeException If an I/O error occurs during reading from the stream
      */
-    public static Json parse(InputStream stream) {
+    public static @NotNull Json parse(@NotNull InputStream stream) {
         try (ByteArrayOutputStream data = new ByteArrayOutputStream()) {
             byte[] buffer = new byte[2048];
 
@@ -100,7 +102,7 @@ public final class JsonParser {
      * @return A {@link Json} object representing the content of the sub-array
      * @throws IllegalArgumentException If the sub-array contains non-positive bytes
      */
-    public static Json parse(byte[] data, int pos, int length) {
+    public static @NotNull Json parse(byte @NotNull [] data, int pos, int length) {
         assert pos >= 0;
         assert length <= data.length;
         byte[] specific = Arrays.copyOfRange(data, pos, length);
@@ -118,7 +120,7 @@ public final class JsonParser {
      * @return A {@link Json} object representing the content of the json string, or null if the string is invalid
      * @throws RuntimeException If an I/O error occurs during parsing
      */
-    public static Json parse(String json) {
+    public static @NotNull Json parse(@Nullable String json) {
         try {
             if (json != null && JsonValidator.isValid(json))
                 return new Json(com.google.gson.JsonParser.parseString(json));
@@ -140,7 +142,7 @@ public final class JsonParser {
      * @param element The {@link JsonElement} to be parsed
      * @return A {@link Json} object representing the content of the {@link JsonElement}
      */
-    public static Json parse(JsonElement element) {
+    public static @Nullable Json parse(@NotNull JsonElement element) {
         if (element.isJsonPrimitive()) {
             JsonPrimitive primitive = element.getAsJsonPrimitive();
             return primitive.isString() ? parse(primitive.getAsString()) : null;

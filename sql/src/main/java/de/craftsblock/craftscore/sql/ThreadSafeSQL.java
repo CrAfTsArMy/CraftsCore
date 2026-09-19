8a7853041c3f22a9e0878b495ef4a01fbe8aa138
Philipp <62473021+CrAfTsArMy@users.noreply.github.com>
2026-09-19 14:08:48 +0200

    feat: Add jdbc subprotocol specification (#9)

--- sql/src/main/java/de/craftsblock/craftscore/sql/ThreadSafeSQL.java
+++ sql/src/main/java/de/craftsblock/craftscore/sql/ThreadSafeSQL.java
@@ -1,5 +1,6 @@
 package de.craftsblock.craftscore.sql;
 
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.Connection;
@@ -13,7 +14,6 @@ import java.sql.SQLException;
  *
  * @author Philipp Maywald
  * @author CraftsBlock
- * @version 1.0.1
  * @see SQL
  * @see de.craftsblock.craftscore.sql.SQL.Callback
  * @since 3.7.24-SNAPSHOT
@@ -58,27 +58,97 @@ public class ThreadSafeSQL extends SQL {
     /**
      * {@inheritDoc}
      * <p>
-     * This method is synchronized to ensure thread-safe connection establishment.
+     * This method is synchronized.
+     *
+     * @deprecated Use {@link #setHost(String)} and {@link #setDatabase(String)} instead
      */
     @Override
-    public synchronized void connect(String user, String password) throws SQLException {
+    @SuppressWarnings("removal")
+    @Deprecated(since = "3.8.19", forRemoval = true)
+    @ApiStatus.ScheduledForRemoval(inVersion = "3.9.0")
+    public synchronized void bind(String host, String database) {
+        super.bind(host, database);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * This method is synchronized.
+     *
+     * @deprecated Use {@link #setHost(String)}, {@link #setPort(int)} and {@link #setDatabase(String)} instead
+     */
+    @Override
+    @SuppressWarnings("removal")
+    @Deprecated(since = "3.8.19", forRemoval = true)
+    @ApiStatus.ScheduledForRemoval(inVersion = "3.9.0")
+    public synchronized void bind(String host, int port, String database) {
+        super.bind(host, port, database);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * This method is synchronized.
+     */
+    @Override
+    public synchronized SQL setHost(@NotNull String host) {
+        return super.setHost(host);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * This method is synchronized.
+     */
+    @Override
+    public synchronized SQL setPort(int port) {
+        return super.setPort(port);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * This method is synchronized.
+     */
+    @Override
+    public synchronized SQL setDatabase(@NotNull String database) {
+        return super.setDatabase(database);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * This method is synchronized.
+     */
+    @Override
+    public synchronized SQL setSubprotocol(@NotNull JdbcSubprotocol subprotocol) {
+        return super.setSubprotocol(subprotocol);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * This method is synchronized.
+     */
+    @Override
+    public synchronized void connect(@NotNull String user, @NotNull String password) throws SQLException {
         super.connect(user, password);
     }
 
     /**
      * {@inheritDoc}
      * <p>
-     * This method is synchronized to ensure thread-safe connection establishment.
+     * This method is synchronized.
      */
     @Override
-    public synchronized void connect(String user, String password, boolean autoReconnect) throws SQLException {
+    public synchronized void connect(@NotNull String user, @NotNull String password, boolean autoReconnect) throws SQLException {
         super.connect(user, password, autoReconnect);
     }
 
     /**
      * {@inheritDoc}
      * <p>
-     * This method is synchronized to ensure thread-safe connection establishment.
+     * This method is synchronized.
      */
     @Override
     public synchronized void disconnect() throws SQLException {
@@ -88,7 +158,7 @@ public class ThreadSafeSQL extends SQL {
     /**
      * {@inheritDoc}
      * <p>
-     * This method is synchronized to ensure thread-safe connection establishment.
+     * This method is synchronized.
      */
     @Override
     public synchronized boolean isConnected() throws SQLException {
@@ -98,58 +168,58 @@ public class ThreadSafeSQL extends SQL {
     /**
      * {@inheritDoc}
      * <p>
-     * This method is synchronized to ensure thread-safe connection establishment.
+     * This method is synchronized.
      */
     @Override
-    public synchronized PreparedStatement prepareStatement(String sql) throws SQLException {
+    public synchronized PreparedStatement prepareStatement(@NotNull String sql) throws SQLException {
         return super.prepareStatement(sql);
     }
 
     /**
      * {@inheritDoc}
      * <p>
-     * This method is synchronized to ensure thread-safe connection establishment.
+     * This method is synchronized.
      */
     @Override
-    public synchronized int update(String sql) throws SQLException {
+    public synchronized int update(@NotNull String sql) throws SQLException {
         return super.update(sql);
     }
 
     /**
      * {@inheritDoc}
      * <p>
-     * This method is synchronized to ensure thread-safe connection establishment.
+     * This method is synchronized.
      */
     @Override
-    public synchronized int update(PreparedStatement statement) throws SQLException {
+    public synchronized int update(@NotNull PreparedStatement statement) throws SQLException {
         return super.update(statement);
     }
 
     /**
      * {@inheritDoc}
      * <p>
-     * This method is synchronized to ensure thread-safe connection establishment.
+     * This method is synchronized.
      */
     @Override
-    public synchronized @NotNull ResultSet query(String query) throws SQLException {
+    public synchronized @NotNull ResultSet query(@NotNull String query) throws SQLException {
         return super.query(query);
     }
 
     /**
      * {@inheritDoc}
      * <p>
-     * This method is synchronized to ensure thread-safe connection establishment.
+     * This method is synchronized.
      */
     @NotNull
     @Override
-    public synchronized ResultSet query(PreparedStatement statement) throws SQLException {
+    public synchronized ResultSet query(@NotNull PreparedStatement statement) throws SQLException {
         return super.query(statement);
     }
 
     /**
      * {@inheritDoc}
      * <p>
-     * This method is synchronized to ensure thread-safe connection establishment.
+     * This method is synchronized.
      */
     @Override
     public synchronized Connection unsafe() {
